061c1dabead943016f15a00fe528fc7c79ea62b5
relfect <larr4kgame@gmail.com>
2023-03-22 23:23:36 +0200

    Update clicker.py

--- clicker.py
+++ clicker.py
@@ -3,7 +3,7 @@ import tkinter as tk
 
 root = tk.Tk()
 root.title("Игра кликер")
-
+root.geometry("300x200")
 
 clicks = 0
 
@@ -13,11 +13,11 @@ def button_click():
     label.config(text=f"Кликов: {clicks}")
 
 
-button = tk.Button(root, text="Кликни меня!", command=button_click)
-button.pack()
+button = tk.Button(root, text="Кликни меня!", command=button_click, bg="blue", fg="white", padx=20, pady=20, borderwidth=0, font=("Arial", 14, "bold"), width=10, height=2, relief="groove")
+button.place(relx=0.5, rely=0.5, anchor="center")
 
 
-label = tk.Label(root, text="Кликов: 0")
+label = tk.Label(root, text="Кликов: 0", font=("Arial", 12))
 label.pack()
 
 
@@ -34,9 +34,12 @@ def change_background():
         current_bg = "white"
 
 
-bg_button = tk.Button(root, text="Сменить фон", command=change_background)
-bg_button.pack()
+bg_button = tk.Button(root, text="Сменить фон", command=change_background, bg="white", fg="black", padx=10, pady=10, borderwidth=0, font=("Arial", 10, "bold"), relief="flat")
+bg_button.place(relx=1, rely=0, anchor="ne")
 
 
 root.mainloop()
+
+
+
 
